7067864c6afb4cd019b27f8d805adc637e2758f2
mitchelljeff <mitchelljeff@hotmail.com>
2017-01-20 15:08:18 +0000

    Update models.py

--- quebap/model/models.py
+++ quebap/model/models.py
@@ -414,5 +414,5 @@ def modelf_predictor(relation, candidate_tuples, candidate_ids, answer_tuple,tar
     batchindex= tf.expand_dims(tf.to_int64(tf.range(tf.shape(candidate_ids)[0])),-1)
     candindex = tf.expand_dims(tf.arg_max(logits,1),-1)
     indexes   = tf.concat(1,[batchindex,candindex])
-    predict = tf.expand_dims(tf.gather_nd(candidate_ids, indexes),axis=1, name='prediction')
+    predict = tf.expand_dims(tf.gather_nd(candidate_ids, indexes),dim=1, name='prediction')
     return logits, loss, predict
